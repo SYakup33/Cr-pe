6ac75552872da36f7126a1f683da01e6d5d1536b
unknown <yakup.senel33@gmail.com>
2022-01-03 10:49:10 +0100

    📦 NEW: initial commit

---
+++ Crepe.php
@@ -0,0 +1,63 @@
+<?php
+    class  Crepe {
+        // Attributs, Propriéte ce sont des variables déclarés dans les class
+        // public, réutilisable à l'intérieur et à l'extérieur de la class
+        // private, réutilisable que à l'intérieur de la class
+        public $oeuf;          //entier qui correspond au nombre d'oeuf
+        public $beurre;        //entier qui correspond à la quantité de beurre gramme de beurre
+        public $lait;          //entier qui correspond à la quantité de lait en litre 
+        public $sucre;         //entier qui correspond à la quantité en cuillère à soupe
+        public $sel;           //entier qui correspond à la quantité de sel pincée de sel
+        public $farine;        //entier qui correspond à la quantité de farine en gramme 
+        public $rhum;          //entier qui correspond à la quantité de cuillère à soupe rhum
+        public $orange;        // entier qui correspond à la quantité d'une orange, 0,5 -> moitié
+        public $nutella;       // entier qui correspond à la quantité de nutella en gramme
+
+        //Méthodes, ce sont des fonctions déclarés dans les class
+        public function __construct( //Méthode qui va être appelée(exécutée) automatiquement à chaque fois qu on va instancier une class
+            $oeuf,
+            $beurre,
+            $lait,
+            $sucre,
+            $sel,
+            $farine,
+            $rhum,
+            $orange,
+            $nutella
+
+        ){
+            //Traitement de votre fonction
+            $this->oeuf = $oeuf;
+            $this->beurre = $beurre;
+            $this->lait = $lait;
+            $this->sucre = $sucre;
+            $this->sel = $sel;
+            $this->rhum = $rhum;
+            $this->farine = $farine;
+            $this->orange = $orange;
+            $this->nutella = $nutella;
+        }
+        
+        public function afficher_recette(){ // Ici on affiche la recette, quantité des ingrédients
+            // Selon si il y'a de l'orange, du nutella ou pas, on affiche le type de crêpe
+            if($this->orange !== 0 && $this->nutella ==0){
+                echo "Dans ma recette de crêpe à l'orange, il y a :\n\n";
+                echo $this->orange." quntité d'une orange.\n";
+            } else if($this->orange == 0 && $this->nutella !=0){
+                echo "Dans ma recette de crêpe au nutella, il y a :\n\n";
+                echo $this->nutella." quantité de nutella en gramme.\n";
+            }else if ($this->orange == 0 && $this->nutella ==0){
+                 echo "Dans ma recette de crêpe simple, il y a :\n\n";
+            }
+            // On affiche les ingrédients ainsi que leur quantité
+            echo $this->oeuf." oeufs.\n";
+            echo $this->beurre." gramme de beurre.\n";
+            echo $this->lait." littre de lait.\n";
+            echo $this->sucre." cuillère à soupe de sucre.\n";
+            echo $this->sel." quantité de sel en gramme .\n"; 
+            echo $this->farine." quantité de farine en gramme.\n";
+            echo $this->rhum." cuillère à soupe de rhum.\n";
+            echo "\n";
+        }
+    }   
+?>
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+    //Fichier différent de Crepe.php
+    //require permet de lier, d'inclure et exécuter le fichier spécifié
+    require './Crepe.php';
+    $crepe_au_nutella = new Crepe(4, 50, 1, 10, 4, 500, 5,0,30); //on indique la quantité des attributs, au nutella
+    $crepe_a_orange = new Crepe(4, 50, 1, 10, 4, 500, 5,0.5,0); //on indique la quantité des attributs, à l'orange
+    $crepe_au_nutella->afficher_recette(); //Exécute la fonction afficher_recette()
+    $crepe_a_orange->afficher_recette(); //Exécute la fonction afficher_recette()
+    // var_dump($crepe_au_nutella);
+    // var_dump($crepe_a_orange);
+    
+?>
